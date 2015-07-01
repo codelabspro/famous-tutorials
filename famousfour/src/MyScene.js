87e8c20dac647f89c66f634f88e38558c4716ae5
Code SDK <codesdktutorials@gmail.com>
2015-07-01 16:29:18 -0400

    Fix scope issue

--- famousfour/src/MyScene.js
+++ famousfour/src/MyScene.js
@@ -16,12 +16,11 @@ function MyScene(selector, data) {
   this.tiles = [];
 
   var farBuildings = this.rootNode.addChild(new CustomNode('url(images/parallax_far_buildings.png)', '#B3E5FC', 0, 0, 1800, 500));
-
+  var plane = this.rootNode.addChild(new CustomNode('url(images/plane_100x100.png)', '#B3E5FC', 200, 200, 100, 100));
   window.addEventListener('keydown', function(e) {
     console.log('keydown occured');
     if (e.keyCode === KeyCodes.RIGHT_ARROW) {
       console.log('Right arrow was pressed');
-      var plane = this.rootNode.addChild(new CustomNode('url(images/plane_100x100.png)', '#B3E5FC', 200, 200, 100, 100));
       var newPosition = Position(plane);
       newPosition.set(1000, 0, -300, { duration: 3000, curve: 'outQuad' });
     }
